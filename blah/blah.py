383bcd900314204865ea2c23e49096370f03c737
Ben Guidarelli <ben.guidarelli@gmail.com>
2023-04-08 21:26:05 -0400

    idk

--- blah/blah.py
+++ blah/blah.py
@@ -1,23 +1,49 @@
+import functools
 from typing import Callable
 import beaker
 import os
 import algokit_utils as au
+import algosdk
 import pynecone as pc
+
+
+def method_caller(
+    app_client: beaker.client.ApplicationClient, method_name: str
+) -> Callable:
+    def _method_caller(_self, *args, **kwargs):
+        app_client.call(method_name, *args, **kwargs)
+
+    _method_caller.__name__ = method_name
+    _method_caller.__qualname__ = method_name
+    return _method_caller
+
+
+def state_val_getter(
+    app_client: beaker.client.ApplicationClient, key: str, val: dict
+) -> pc.Var:
+    def _variable_getter(_self):
+        # TODO: get this once for all state
+        return app_client.get_global_state().get(
+            key, 0 if val["type"] == "uint64" else ""
+        )
+
+    _variable_getter.__name__ = key
+    _variable_getter.__qualname__ = key
+    return pc.var(_variable_getter)
 
 
 class PCApp:
     def __init__(
-        self, app_client: beaker.client.ApplicationClient, create: bool = True
+        self, app_client: beaker.client.ApplicationClient, auto_create: bool = True
     ):
         self.account = (app_client.sender, app_client.signer)
         self.app_client = app_client
         self.app_spec = app_client._app_client.app_spec
 
-        # Only create the app once
         if self.app_client.app_id == 0:
             if env_app_id := os.getenv("PC_APP_ID") is not None:
                 self.app_client.app_id = int(env_app_id)
-            elif create:
+            elif auto_create:
                 app_id, _, _ = self.app_client.create()
                 app_client.app_id = app_id
                 os.environ["PC_APP_ID"] = str(app_id)
@@ -33,80 +59,105 @@ class PCApp:
         )
         return PCApp(app_client)
 
-    def get_state_fields(self) -> dict:
-        def variable_getter(key: str, default_value) -> pc.Var:
-            def _variable_getter(_self):
-                return self.app_client.get_global_state().get(key, default_value)
+    def app_state(self) -> type[pc.State]:
+        fields = pcap.get_states() | pcap.get_action_states()
+        return type("ok", (pc.State,), fields)
 
-            _variable_getter.__name__ = key
-            _variable_getter.__qualname__ = key
-            return pc.var(_variable_getter)
+    def get_states(self) -> dict:
+        fields: dict = {}
+        for k, v in self.app_spec.schema["global"]["declared"].items():
+            fields[k] = state_val_getter(self.app_client, k, v)
+        return fields
 
-        def method_caller(method_name: str) -> Callable:
-            def _method_caller(_self):
-                self.app_client.call(method_name)
-
-            _method_caller.__name__ = method.name
-            _method_caller.__qualname__ = method.name
-            return _method_caller
-
+    def get_action_states(self) -> dict:
         fields: dict = {}
         for method in self.app_spec.contract.methods:
             if method.name == "create":
                 continue
-            fields[method.name] = method_caller(method.name)
-
-        for k, v in self.app_spec.schema["global"]["declared"].items():
-            fields[k] = variable_getter(
-                key=k, default_value=0 if v["type"] == "uint64" else ""
-            )
+            fields[method.name] = method_caller(self.app_client, method.name)
 
         return fields
 
-    def get_actions(self) -> list:
-        buttons = []
+    def get_actions(self, ctx: type[pc.State]) -> list:
+        actions = []
         for method in pcap.app_spec.contract.methods:
             if method.name == "create":
-                continue
-            buttons.append(
+               continue
+
+            args_key = f"{method.name}.args"
+            fields = {
+               args_key: [a.dictify() for a in method.args],
+            }
+
+            MethodState = type("MethodState", (ctx,), fields)
+
+            def tx_input(arg):
+                print(arg.__dict__)
+                return pc.input(
+                        placeholder=arg["name"],
+                        #_type= "int" if str(arg.type) == "uint64" else "str",
+                        width="100%",
+                        outline="black",
+                    )
+
+            args = pc.foreach(getattr(MethodState, args_key), tx_input)
+
+            send_tx = functools.partial(getattr(AppState, method.name), )
+            
+            action = pc.hstack(
+                args,
                 pc.button(
                     method.name,
                     border_radius="1em",
-                    on_click=getattr(State, method.name),
-                )
+                    on_click=send_tx,
+                ),
             )
-        return buttons
+
+            actions.append(action)
+        return actions
 
     def get_global_state(self) -> list:
-        statevals = []
+        global_state = []
         for k, v in pcap.app_spec.schema["global"]["declared"].items():
-            statevals.append(pc.heading(getattr(State, k), font_size="2em"))
+            gsv = pc.heading(f"{k} | " + getattr(AppState, k), font_size="2em")
 
-        return statevals
+            global_state.append(gsv)
+        return global_state
 
     def get_local_state(self) -> list:
-        statevals = []
+        local_state = []
         for k, v in pcap.app_spec.schema["local"]["declared"].items():
-            statevals.append(pc.heading(getattr(State, k), font_size="2em"))
-
-        return statevals
+            local_state.append(pc.heading(getattr(AppState, k), font_size="2em"))
+        return local_state
 
     def dapp_flow(self):
         return f"https://app.dappflow.org/explorer/application/{self.app_client.app_id}/transactions"
 
 
-p = "blah/application/artifacts/application.json"
+## p = "blah/application/artifacts/application.json"
+p = "/home/ben/beaker/examples/amm/ConstantProductAMM.artifacts/application.json"
 pcap = PCApp.from_app_spec(p)
-
+AppState = pcap.app_state()
 
 def index():
     return pc.vstack(
-        pc.hstack(*pcap.get_actions(), *pcap.get_global_state()),
+        *pcap.get_actions(AppState),
+        *pcap.get_global_state(),
         pc.link("DappFlow", href=pcap.dapp_flow(), is_external=True),
     )
 
+#class State(pc.State):
+#    x: int = 123
+#
+#class ChildState(State):
+#    x: int = 456
+#
 
-State = type("State", (pc.State,), pcap.get_state_fields())
-app = pc.App(state=State)
+#def index():
+#    return pc.vstack(
+#        pc.heading(ChildState.x, font_size="2em"),
+#    )
+
+app = pc.App(state=AppState)
 app.add_page(index)
 app.compile()
